a59137ce6223665e473343fa3635ec55c3472817
Jason Solis <jsolis@gmail.com>
2014-12-29 15:08:50 -0500

    updated serialport parser to use readline and look for a new line char

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-var SerialPort = require("serialport").SerialPort;
-var serialport = new SerialPort("/dev/tty.usbserial-DA00UJER");
+var serialport = require("serialport");
+var SerialPort = serialport.SerialPort;
+var serialport = new SerialPort("/dev/tty.usbserial-DA00UJER", {
+  parser: serialport.parsers.readline("\n")
+});
+
 serialport.on('open', function(){
   console.log('Serial Port Opend');
+
   serialport.on('data', function(data){
-    console.log(data[0]);
-    if (data[0] == 1) {
-      console.log('making call to build');
-    } else if (data[0] == 2) {
-      console.log('making call to deploy');
+    console.log('data: ' + data);
+
+    data = data + '';
+
+    if (data === 'button1') {
+      console.log('making call for button1');
+    } else if (data === 'button2') {
+      console.log('making call for button2');
     }
   });
 });
